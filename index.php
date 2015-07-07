fd780e67ab23bd3dd13ae423cb1217427a44af01
Jake Aldred <jakealdred@me.com>
2015-07-07 21:45:22 +0100

    add index

--- index.php
+++ index.php
@@ -7,9 +7,7 @@ session_start();
 <html>
     <head>
         <title>Future Aspirations</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="style.css">
+         <?php include_once "header.php"; ?>
     </head>
     <body> 
 
