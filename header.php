f1bc29439ea2ed80bdf90f664143ea6d0fa2e8d8
Jake Aldred <jakealdred@me.com>
2015-07-07 21:54:19 +0100

    add header

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style>
+    	body {
+    	padding-top: 60px;
+    	}
+    	
+    </style>
+  </head>
+  <body>
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="bootstrapTest.php">Future Aspirations</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-left">
+                <li><a href="bootstrapTest.php">Home</a></li>
+                <li><a href="GroupWorkRecording.php">Group Work Recording</a> </li>
+                <li><a href="StreetWorkRecording.php">Street Work Recording</a></li>
+                <li><a href="TimeSheetSubmission.php">Time Sheet Submission</a></li>
+                <li><a href="VolunteerTimesheet.php">Volunteer Timesheet</a></li>
+                <li><a href="registerPage.php">Register</a></li>
+                <li><a href="#">Contact</a></li> 
+              </ul>
+        </div>
+      </div>
+    </div>
+	</body>
+</html>
