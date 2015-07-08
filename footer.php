eca6c1121db8e4b069ece8d736498e856eba8af4
Jake Aldred <jakealdred@me.com>
2015-07-08 22:42:37 +0100

    Landing page

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
+    <link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="landing-page.css" rel="stylesheet">
+    </head>
+	<body>
+	<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#about">Register</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted small">Copyright &copy; Future Aspirations (Scotland) Ltd 2015. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+        </footer>
+    </body>
+</head>
---
+++ landingPage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Future Aspirations</title>
+		<?php include_once "header.php"; ?>
+		<link href="landing-page.css" rel="stylesheet">
+	</head>
+	<body>
+		<a name="about"></a>
+    	<div class="intro-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Future Aspirations</h1>
+                        <h3>We would like to welcome you to this site, which is very much still under construction. We are a social enterprise covering Scotland. We aim to develop and build the capacity of local organisations to deliver high quality youth work interventions and look forward to hearing from you if you need our services.
+							Please click on the links above to navigate your way through the site.
+						<br>
+							- John Gilfillan Chief Executive Officer</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="https://twitter.com/FutureAspire" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/FutureAspirations" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   		<script src="http://code.jquery.com/jquery.js"></script>
+    	<script src="bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+  	</body>
+  	<?php include_once "footer.php"; ?>
+</html>
